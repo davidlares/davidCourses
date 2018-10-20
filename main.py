a1709d5a138c12d427f0f10e481b56a7697a7cc1
David Lares <david.e.lares@gmail.com>
2018-10-20 14:32:23 -0400

    creating a dummy data for GET API method, DB connections on every request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import g # database on request
 
 from models import DATABASE
 from models import initialize
@@ -8,9 +9,23 @@ app = Flask(__name__)
 PORT = 8000
 DEBUG = True
 
+# open a DB connection
+@app.before_request
+def before_request():
+    g.db = DATABASE
+    g.db.connect()
+
+# close the DB connection
+@app.after_request
+def after_request(request):
+    g.db.close()
+    return request
+
 @app.route('/api/v1/courses', methods=['GET'])
 def get_courses():
-    return "Hello, world"
+    courses = Course.select() # select * from courses
+    # print(courses)
+    return "Already got the courses"
 
 if __name__ == "__main__":
     # running the model function
--- models.py
+++ models.py
@@ -14,7 +14,15 @@ class Course(Model):
     description = TextField()
     created_at = DateTimeField(default = datetime.datetime.now())
 
+# dummy data method - GET method instead of POST
+def create_course():
+    title = 'Flask Course'
+    description = 'Free Flask Course'
+    if not Course.select().where(Course.title == title):
+        Course.create(title = title, description = description)
+
 def initialize():
     DATABASE.connect()
     DATABASE.create_tables([Course], safe=True) # should be extending Model -> that comes from peewee
+    create_course()
     DATABASE.close()
