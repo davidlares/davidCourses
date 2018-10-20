3652cacf472cc5f7e21d0ab1223b0678aded9bb9
David Lares <david.e.lares@gmail.com>
2018-10-20 14:20:59 -0400

    Fixing Typos and model setup

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+
+from models import DATABASE
+from models import initialize
+from models import Course
+
 app = Flask(__name__)
 PORT = 8000
 DEBUG = True
 
 @app.route('/api/v1/courses', methods=['GET'])
 def get_courses():
-    pass
+    return "Hello, world"
 
 if __name__ == "__main__":
+    # running the model function
+    initialize()
     app.run(port = PORT, debug = DEBUG)
--- models.py
+++ models.py
@@ -1,13 +1,20 @@
-import peewee import * #ORM
+from peewee import * #ORM
 import datetime
 
 # DB string connection
-DATABASE = MySQLDatabase('PyRest',host="localhost", user="root", passwd="");
+DATABASE = MySQLDatabase('PyRest',host="localhost", user="root", passwd="admin");
 
 class Course(Model):
     class Meta:
+        # database assignation
         database = DATABASE
+        db_table = 'courses'
 
     title = CharField(unique=True, max_length=250)
     description = TextField()
-    created_at = DateTimeField(default=datetime.datetime.now)
+    created_at = DateTimeField(default = datetime.datetime.now())
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Course], safe=True) # should be extending Model -> that comes from peewee
+    DATABASE.close()
